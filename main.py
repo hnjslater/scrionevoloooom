f62543ef95cd148bb6200454d75efc062e6df80f
HNJSlater <henry.slater@gmail.com>
2013-12-15 00:02:41 +0000

    bullets and baddies, feels like a real game now!

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 import random
 from constants import *
 import constants
+from random import randint
 
 
 #functions to create our resources
@@ -22,7 +23,6 @@ def load_image(name, colorkey=None):
 class Goodie(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        pygame.sprite.Sprite.__init__(self) #call Sprite intializer
         self.image, self.rect = load_image('Brewdog-Logo-blue.png', -1)
         screen = pygame.display.get_surface()
         self.area = screen.get_rect()
@@ -31,34 +31,61 @@ class Goodie(pygame.sprite.Sprite):
 
 
     def start_moving_left(self):
-        self.dx = 4;
+        self.dx = 8;
 
     def stop_moving_left(self):
-        if self.dx == 4:
+        if self.dx == 8:
             self.dx = 0;
 
     def start_moving_right(self):
-        self.dx = -4;
+        self.dx = -8;
 
     def stop_moving_right(self):
-        if self.dx == -4:
+        if self.dx == -8:
             self.dx = 0;
+
+    def fire_bullets(self):
+        return (Bullet(self.rect.left, self.rect.top), Bullet(self.rect.right, self.rect.top))
+        
 
     def update(self, *arg):
         new_left = self.rect.left + self.dx
         if new_left > 0 and new_left + self.rect.width < SCREEN_SIZE:
             self.rect.left = new_left
-        
 
-        
+
+class Baddie(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('Stella-Artois-chalice.png', -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = x, y
+        self.dx = 0
+    def update(self, *arg):
+        self.rect.top = self.rect.top + 10
+        if self.rect.top > SCREEN_SIZE:
+            self.kill()
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image, self.rect = load_image('Brewdog-Logo-blue-small.png', -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = x, y
+    def update(self, *arg):
+        self.rect.top = self.rect.top - 30
+        if self.rect.top < 0:
+            self.kill()
+
+
 
 def main():
 	pygame.init()
 
         #pygame.mixer.music.load('backing_music.ogg')
         #pygame.mixer.music.play(-1, 0.0)
-
-
 
 	fpsClock = pygame.time.Clock()
 
@@ -70,9 +97,15 @@ def main():
         goodies.append(Goodie())
         
         allsprites = pygame.sprite.RenderPlain(goodies)
+        bullets = pygame.sprite.RenderPlain()
+        baddies = pygame.sprite.RenderPlain()
+        baddies.add(Baddie(10,10))
+        baddies.add(Baddie(50,10))
+        baddies.add(Baddie(400,10))
 	while True:
             win.fill(pygame.Color(0,0,0))
-            
+
+
             
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -84,6 +117,8 @@ def main():
                             goodies[0].start_moving_left()
                     if event.key == K_h:
                             goodies[0].start_moving_right()
+                    if event.key == K_SPACE:
+                            bullets.add(goodies[0].fire_bullets())
                 elif event.type == KEYUP:
                     if event.key == K_l:
                             goodies[0].stop_moving_left()
@@ -96,9 +131,19 @@ def main():
             allsprites.update(win)
             allsprites.draw(win)
 
+            bullets.update(win)
+            bullets.draw(win)
+
+            baddies.update(win)
+            baddies.draw(win)
+
+            pygame.sprite.groupcollide(bullets, baddies, True, True)
+
+            if len(baddies) < 3:
+                baddies.add(Baddie(randint(0,SCREEN_SIZE),20))
 
             pygame.display.update()
-            fpsClock.tick(30)
+            fpsClock.tick(60)
 
 
 
